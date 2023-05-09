613fea5c237c075a1f6bb807dafd9016b699944e
Rhythm-Jain-42 <rhythmjain101@gmail.com>
2023-05-09 13:58:54 +0530

    talkie commit

---
+++ Desktop/React_Projects/talkie/src/App.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import Navbar from './components/NavBar';
+// const style = {
+//   appContainer: `max-w-[728px] mx-auto text-center`,
+//   sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+// };
+
+// function App  ()  {
+//   return (
+//     <div className={style.appContainer}>
+//       <section className={style.sectionContainer}>
+//         <NavBar/>
+//       </section>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
